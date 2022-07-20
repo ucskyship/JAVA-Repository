aad3b9192e89a1c4e35bbec3f6460d83d2e18333
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-07-20 12:09:42 +0100

    first maven project

---
+++ src/africa/semicolon/trueCaller/data/models/Contact.java
@@ -0,0 +1,50 @@
+package africa.semicolon.trueCaller.data.models;
+
+public class Contact {
+    private int id;
+    private  String firstName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private  String secondName;
+    private  String phoneNumber;
+    private  String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/africa/semicolon/trueCaller/data/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package africa.semicolon.trueCaller.data.repositories;
+
+import africa.semicolon.trueCaller.data.models.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    User saveUser(User user);
+    void delete(User user);
+    void delete(int id);
+    User findById(int id);
+    int count();
+    List<User> findAll();
+    User searchUser(String username);
+}
